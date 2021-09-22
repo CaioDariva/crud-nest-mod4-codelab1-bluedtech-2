548a9d3dce18f9965b62efdd589b1d3b2cc69a9e
Caio <cgmalavazidariva@gmail.com>
2021-09-22 00:20:30 -0300

    try to put MongoDB Schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://caiodariva:pBxrCLuVONenlC9m@caiomongo.5kpn2.mongodb.net/Modulo4?retryWrites=true&w=majority',
+    ),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
